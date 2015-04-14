62e555b73387bd292f089cd7a22d5a9d70be27ec
Yair <yair.rodriguez@negoapps.com>
2015-04-14 16:44:49 -0500

    Practica Touchmove

---
+++ app/controllers/index.js
@@ -0,0 +1,13 @@
+
+
+
+
+$.view.addEventListener('touchmove', function(e){
+	var deltaX = e.x;
+	var deltaY = e.y;
+	Ti.API.info("cursor en x: ",e.x);
+	Ti.API.info("cursor en y: ",e.y);
+});
+
+
+$.index.open();
