3ba366c6ae147c7ecf7bc4965e85a55782b328d0
Yair <yair.rodriguez@negoapps.com>
2015-04-20 00:32:38 -0500

    update

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,12 +2,13 @@
 
 
 
-$.view.addEventListener('touchmove', function(e){
-	var deltaX = e.x;
-	var deltaY = e.y;
-	Ti.API.info("cursor en x: ",e.x);
-	Ti.API.info("cursor en y: ",e.y);
-});
 
+
+$.index.addEventListener('touchmove', function(e){
+	Ti.API.info('x: '+e.x + ', y: '+ e.y);
+	
+	
+	$.lblxy.text = "x: "+e.x+",y: "+e.y;
+});
 
 $.index.open();
